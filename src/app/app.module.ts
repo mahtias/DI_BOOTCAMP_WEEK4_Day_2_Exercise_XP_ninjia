695df646bdd40cb5318c0ad4a8e1650bd1fe3c17
mahtias <kadjomathias1994@gmail.com>
2023-01-17 15:57:21 +0000

    premier depot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+//import { ProductListComponent } from './module-list-product/product-list/product-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+
   ],
   imports: [
     BrowserModule,
